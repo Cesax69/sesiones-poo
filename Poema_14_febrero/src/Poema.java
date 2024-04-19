8a5477055f4813f63e82c5ae447d40549cf3da90
Cesax69 <114587045+Cesax69@users.noreply.github.com>
2024-04-18 23:23:56 -0600

    Add files via upload

---
+++ Poema_14_febrero/src/Poema.java
@@ -0,0 +1,19 @@
+public class Poema {
+    
+    /**
+     * @Author Cesar Enrique Garay Garcia 
+     * @Category GDS0622
+     * @since 16/02/24
+     */
+    public static void main(String[] args) {
+        // Imprime un poema para el 14 de febrero
+        System.out.println("En este día de amor y dulzura,");
+        System.out.println("te entrego mi corazón con ternura.");
+        System.out.println("Eres mi razón para sonreír,");
+        System.out.println("mi compañero fiel, mi mejor existir.");
+        System.out.println("Juntos en este camino de pasión,");
+        System.out.println("celebramos nuestro eterno lazo de unión.");
+        System.out.println("Feliz San Valentín, mi amor querido,");
+        System.out.println("contigo siempre, en cada latido.");
+    }
+}
